c7ba058c4fb534c6ac3dc0789d23849e63ddb651
ayush-rawat-bw <ayush@thebetterway.co>
2025-09-05 10:19:50 +0530

    Initial Setup

---
+++ src/db/dexie.js
@@ -0,0 +1,28 @@
+import Dexie from 'dexie';
+
+// Create your database
+export const db = new Dexie('ClinicVisitDB');
+
+// Define schema
+db.version(1).stores({
+
+});
+
+// example for stores obj 
+// {
+//     food: '++id, name, quantity, price'
+// }
+
+
+// Example functions
+export const addVisit = async (checkInTime) => {
+    // ADD CODE HERE
+};
+
+export const checkOutVisit = async (id, checkOutTime, notes) => {
+    // ADD CODE HERE
+};
+
+export const getAllVisits = async () => {
+    // ADD CODE HERE
+};
